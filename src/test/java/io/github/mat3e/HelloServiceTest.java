fcdcd1ef12535d6dbcfdb15360ce083e9e034086
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-05-24 15:21:57 +0200

    Added flyway plugin and h2 dependency to pom. Created 2 versions of db with languages - flyway migration. Added some unit tests.

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -2,23 +2,85 @@ package io.github.mat3e;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HelloServiceTest {
-
-    private HelloService SUT = new HelloService();
+    private final static String WELCOME = "Hello";
+    private final static String FALLBACK_ID_WELCOME = "Hola";
 
     @Test
-    public void testNullPrepareGreetingReturnsGreetingWithFallbackValue() throws Exception {
-       String result=SUT.prepareGreeting(null);
-       assertEquals("Hello "+HelloService.FALLBACK_NAME+"!",result);
+    public void testNullNamePrepareGreetingReturnsGreetingWithFallbackValue() throws Exception {
+
+        LangRepository mockRepository = alwaysReturningHelloRepository();
+        HelloService SUT = new HelloService(mockRepository);
+        String result = SUT.prepareGreeting(null, "-1");
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
     }
+
+    private LangRepository alwaysReturningHelloRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.of(new Lang(null, WELCOME, null));
+            }
+        };
+    }
+
     @Test
     public void testNamePrepareGreetingReturnsGreetingWithName() throws Exception {
-        String test="test";
-        String result=SUT.prepareGreeting(test);
-        assertEquals("Hello "+test+"!",result);
+        LangRepository mockRepository = alwaysReturningHelloRepository();
+        HelloService SUT = new HelloService(mockRepository);
+        String name = "test";
+        String result = SUT.prepareGreeting(name, "-1" + "");
+        assertEquals(WELCOME + " " + name + "!", result);
+    }
+
+    @Test
+    public void testNullLangPrepareGreetingReturnsGreetingWithFallbackIdLang() throws Exception {
+        LangRepository mockRepository = fallbackLangIdRepository();
+        HelloService SUT = new HelloService(mockRepository);
+        String result = SUT.prepareGreeting(null, "abc");
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void testTextLangPrepareGreetingReturnsGreetingWithFallbackIdLang() throws Exception {
+        LangRepository mockRepository = fallbackLangIdRepository();
+        HelloService SUT = new HelloService(mockRepository);
+        String result = SUT.prepareGreeting(null, null);
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    private LangRepository fallbackLangIdRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                if (id.equals(HelloService.FALLBACK_LANG.getId())) {
+                    return Optional.of(new Lang(null, FALLBACK_ID_WELCOME, null));
+                }
+                return Optional.empty();
+            }
+        };
+    }
+
+    @Test
+    public void testNonExistingLangPrepareGreetingReturnsGreetingWithFallbackLang() throws Exception {
+        LangRepository mockRepository = fallbackLangRepository();
+        HelloService SUT = new HelloService(mockRepository);
+        String result = SUT.prepareGreeting(null, "-1");
+        assertEquals(HelloService.FALLBACK_LANG.getWelcomeMsg() + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    private LangRepository fallbackLangRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.empty();
+            }
+        };
     }
 
 }
