4ba8eb045ef0582bd0b93c9ba7844a23d164dc00
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-05-27 14:10:07 +0200

    LangServlet added.

--- src/main/java/io/github/mat3e/LangRepository.java
+++ src/main/java/io/github/mat3e/LangRepository.java
@@ -9,14 +9,26 @@ import java.util.Optional;
 
 class LangRepository {
 
+    List<Lang> findAll(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<Lang> result = session.createQuery("from Lang", Lang.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
 
     Optional<Lang> findById(Integer id) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        Lang result = session.get(Lang.class, id);
+
+        Optional result = Optional.ofNullable(session.get(Lang.class, id));
+
         transaction.commit();
         session.close();
-        return Optional.ofNullable(result);
+        return result;
     }
 
 
